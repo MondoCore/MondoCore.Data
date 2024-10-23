356b242768d96dd509991e0b99384aa51d1b44b1
Jim Lightfoot <jim.lightfoot@outlook.com>
2024-10-22 17:45:12 -0700

    Add even more

---
+++ MondoCore.Data/Interfaces/IDatabase.cs
@@ -0,0 +1,30 @@
+﻿/***************************************************************************
+ *                                                                          
+ *    The MondoCore Libraries  	                                            
+ *                                                                          
+ *      Namespace: MondoCore.Data	                                        
+ *           File: IDatabase.cs                                                
+ *      Class(es): IDatabase                                                   
+ *        Purpose: Interface to query a database                               
+ *                                                                          
+ *  Original Author: Jim Lightfoot                                         
+ *    Creation Date: 24 Feb 2021                                           
+ *                                                                          
+ *   Copyright (c) 2021-2024 - Jim Lightfoot, All rights reserved                
+ *                                                                          
+ *  Licensed under the MIT license:                                         
+ *    http://www.opensource.org/licenses/mit-license.php                    
+ *                                                                          
+ ****************************************************************************/
+
+namespace MondoCore.Data
+{
+    /// <summary>
+    /// Interface for a database
+    /// </summary>
+    public interface IDatabase
+    {
+        IReadRepository<TID, TValue> GetRepositoryReader<TID, TValue>(string repoName, IIdentifierStrategy<TID> strategy = null) where TValue : IIdentifiable<TID>;
+        IWriteRepository<TID, TValue> GetRepositoryWriter<TID, TValue>(string repoName, IIdentifierStrategy<TID> strategy = null) where TValue : IIdentifiable<TID>;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MondoCore.Data
-Interfaces, extensions and classes for repositories (databases and collections)
+
+<br>
+
+A collection of interfaces and classes for accessing data from databases and other data sources
+
+
+## Table of Contents
+- [Interfaces](#interfaces)
+- [Classes](#classes)
+
+<br/>
+
+<a name="interfaces"></a>
+## Interfaces
+
+<br/>
+
+<a name="classes"></a>
+## Classes 
+
+<br>
+
